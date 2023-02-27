55a6d388ffeedb177bc9b7dc7499b8abba155b57
jefenso <jaespiritu@student.apc.edu.ph>
2023-02-27 19:38:02 +0800

    codeigniter convert

--- week9/GALLERY.php
+++ week9/GALLERY.php
@@ -5,49 +5,8 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Audiowide|Sofia|Trirong">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-colors-flat.css">
-<style>
- h1.a {
-  font-family: "Audiowide", sans-serif;
-  color: beige;
-}
- h2.b {
-  font-family: "Audiowide", sans-serif;
-  color: rgb(48, 65, 224);
- }
- .glow {
-  font-family: "Audiowide", sans-serif;
-  font-size: 80px;
-  color: #fff;
-  text-align: center;
-  animation: glow 1s ease-in-out infinite alternate;
-}
-.glow2 {
-  font-family: "Audiowide", sans-serif;
-  font-size: 20px;
-  color: #fff;
-  text-align: center;
-  animation: glow 1s ease-in-out infinite alternate;
-}
-.glow3 {
-  font-family: "Audiowide", sans-serif;
-  font-size: 40px;
-  color: #fff;
-  text-align: center;
-  animation: glow 1s ease-in-out infinite alternate;
-}
-@-webkit-keyframes glow {
-  from {
-    text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #e60073, 0 0 40px #e60073, 0 0 50px #e60073, 0 0 60px #e60073, 0 0 70px #e60073;
-  }
-  
-  to {
-    text-shadow: 0 0 20px #fff, 0 0 30px #ff4da6, 0 0 40px #ff4da6, 0 0 50px #ff4da6, 0 0 60px #ff4da6, 0 0 70px #ff4da6, 0 0 80px #ff4da6;
-  }
-}
-
-</style>
-
-  <link rel="icon" type="image/x-icon" href="https://static.wikia.nocookie.net/honkaiimpact3_gamepedia_en/images/0/05/Flame-Chasers%27_Triumph.png/revision/latest/scale-to-width-down/256?cb=20220916154731">
+<link rel="stylesheet" href="style.css">
+<link rel="icon" type="image/x-icon" href="https://static.wikia.nocookie.net/honkaiimpact3_gamepedia_en/images/0/05/Flame-Chasers%27_Triumph.png/revision/latest/scale-to-width-down/256?cb=20220916154731">
 </head>
 
 <body>
--- week9/index.php
+++ week9/index.php
@@ -4,49 +4,8 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Audiowide|Sofia|Trirong">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-colors-flat.css">
-<style>
- h1.a {
-  font-family: "Audiowide", sans-serif;
-  color: beige;
-}
- h2.b {
-  font-family: "Audiowide", sans-serif;
-  color: rgb(48, 65, 224);
- }
- .glow {
-  font-family: "Audiowide", sans-serif;
-  font-size: 80px;
-  color: #fff;
-  text-align: center;
-  animation: glow 1s ease-in-out infinite alternate;
-}
-.glow2 {
-  font-family: "Audiowide", sans-serif;
-  font-size: 20px;
-  color: #fff;
-  text-align: center;
-  animation: glow 1s ease-in-out infinite alternate;
-}
-.glow3 {
-  font-family: "Audiowide", sans-serif;
-  font-size: 40px;
-  color: #fff;
-  text-align: center;
-  animation: glow 1s ease-in-out infinite alternate;
-}
-@-webkit-keyframes glow {
-  from {
-    text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #e60073, 0 0 40px #e60073, 0 0 50px #e60073, 0 0 60px #e60073, 0 0 70px #e60073;
-  }
-  
-  to {
-    text-shadow: 0 0 20px #fff, 0 0 30px #ff4da6, 0 0 40px #ff4da6, 0 0 50px #ff4da6, 0 0 60px #ff4da6, 0 0 70px #ff4da6, 0 0 80px #ff4da6;
-  }
-}
-
-</style>
-
-  <link rel="icon" type="image/x-icon" href="https://static.wikia.nocookie.net/honkaiimpact3_gamepedia_en/images/0/05/Flame-Chasers%27_Triumph.png/revision/latest/scale-to-width-down/256?cb=20220916154731">
+<link rel="stylesheet" href="style.css">
+<link rel="icon" type="image/x-icon" href="https://static.wikia.nocookie.net/honkaiimpact3_gamepedia_en/images/0/05/Flame-Chasers%27_Triumph.png/revision/latest/scale-to-width-down/256?cb=20220916154731">
 </head>
 
 <body>
@@ -73,31 +32,6 @@
     <h1 class="glow"><h1 class="w3-xxlarge w3-text-white"><span class="w3-padding w3-black w3-opacity-min"><b>My name is Jeff Enzo Espiritu and I am a software developer.</b></span></h1>
 
 
-<style>
-    body {
-      background-repeat: no-repeat;background-size: cover; background-image: url("guwa.gif");
-      background-attachment: fixed;
-      
-        }
-        p.third {
-        background: rgba(255, 23, 104, 0.6);
-      }
-  
-
-.container{
-  position: relative;
-  max-width: 800px;
-  margin: 0 auto;
-  padding: 5vh 0vh 5vh 0vh;
-
-}
-#grad1 {
-  height: 200px;
-  background-color: red; /* For browsers that do not support gradients */
-  background-image: linear-gradient(to right, red , yellow);
-}
-
-</style>
 <div class=".container">
   <h3 class="glow3">About me</h3>
 </div>
